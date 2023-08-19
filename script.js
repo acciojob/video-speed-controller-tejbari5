f5eb5bb728af66826456ef523f085a6449898572
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2023-08-19 09:24:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function updateProgressBar() {
+  const progressPercentage = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${progressPercentage}%`;
+}
+
+video.addEventListener('timeupdate', updateProgressBar);
+
+// Skipping functionality
+function skip() {
+  const skipAmount = parseFloat(this.dataset.skip);
+  video.currentTime += skipAmount;
+}
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+// Volume and playback speed functionality
+function handleVolumeChange() {
+  video.volume = this.value;
+}
+
+function handlePlaybackSpeedChange() {
+  video.playbackRate = this.value;
+  speedBar.textContent = this.value + 'x';
+}
+
+volumeSlider.addEventListener('change', handleVolumeChange);
+volumeSlider.addEventListener('mousemove', handleVolumeChange);
+
+playbackSpeedSlider.addEventListener('change', handlePlaybackSpeedChange);
+playbackSpeedSlider.addEventListener('mousemove', handlePlaybackSpeedChange);
